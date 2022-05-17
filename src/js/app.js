feab17c0f5505790f354b687c59a8c192b40ce31
Lily <brezdina@ukr.net>
2022-05-17 18:07:28 +0300

    smartbasket

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 import * as flsFunctions from "./modules/functions.js";
 import * as swiper from "./modules/swiper-bundle.js";
 import * as swiper_init from "./modules/swiper-init.js";
-import * as map_init from "./modules/map-init.js";
+
 import * as smartbasket from "./modules/smartbasket.js";
 import * as smartbasket_init from "./modules/smartbasket-init.js";
 import * as smartbasket_alert from "./modules/smartbasket-alert.js";
+import * as map_init from "./modules/map-init.js";
 
 flsFunctions.isWebp();
 
