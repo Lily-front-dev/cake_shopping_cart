a1098687557f62ac06173ea5fd381a1c4dd7796b
Lily <brezdina@ukr.net>
2022-06-24 21:10:45 +0300

    added wow animate

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+
 var jquery = require('jquery');
 import * as smartbasket_main from "./modules/smartbasket/smartbasket.min.js";
 import * as smartbasket_alert from "./modules/smartbasket/smartbasket-alert.js";
@@ -8,7 +9,5 @@ import * as flsFunctions from "./modules/functions.js";
 import * as swiper from "./modules/swiper/swiper-bundle.js";
 import * as swiper_init from "./modules/swiper/swiper-init.js";
 import * as map_init from "./modules/map/map-init.js";
-
-
 flsFunctions.isWebp();
 
