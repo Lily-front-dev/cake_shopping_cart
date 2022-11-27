570d764503beca259cc5c6d56125951d3f7cdf59
Lily <brezdina@ukr.net>
2022-11-27 16:28:16 +0200

    minor updates

--- src/js/app.js
+++ src/js/app.js
@@ -13,14 +13,40 @@ import * as flsFunctions from "./modules/functions.js";
 import * as swiper from "./modules/swiper/swiper-bundle.js";
 import * as swiper_init from "./modules/swiper/swiper-init.js";
 
-/*Map*/
-import * as map_init from "./modules/map/map-init.js";
+/*Map
+import * as map_init from "./modules/map/map-init.js";*/
+
+/*Bootstrap*/
+/*import * as jquery from "./modules/bootstrap/jquery.min.js";
+import * as poper from "./modules/bootstrap/popper.min.js";
+import * as bootstrapjs from "./modules/bootstrap/bootstrap.min.js";*/
+
+/*import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";*/
 
 /*WowAnimate*/
 /*import * as wowanimate_js from "./modules/wowanimate/wow.js";*/
 /*import * as wowanimate_js1 from "./modules/wowanimate/wow.min.js";*/
 /*import * as wowanimate_init from "./modules/wowanimate/wowanimate.js";*/
 
+// Import jQuery
+import * as jquery from "../../node_modules/jquery/dist/jquery.js";
+// Import Popper
+import * as popper from "../../node_modules/popper.js/dist/umd/popper.js";
+// Import Bootstrap js
+import * as bootstrapjs from "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/alert.js
+//= ../../../node_modules/bootstrap/js/dist/button.js
+//= ../../../node_modules/bootstrap/js/dist/carousel.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../../node_modules/bootstrap/js/dist/tab.js
+//= ../../../node_modules/bootstrap/js/dist/toast.js
 flsFunctions.isWebp();
 
 
